ce061b9a343472c3c14096fb9d9448f9e32d2ef4
sibiraj -20 <sibirajm.20cse@kongu.edu>
2021-10-23 11:47:45 +0530

    Insurance Commit

---
+++ Insdetail.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main ()
+{
+char sname,nname,rn,nom_oc,nemail;
+int nage,n_no,n_aa;
+printf("%c Student Name : SIBIRAJ M",sname);
+printf("%c Nominee Name : MOHANKUMAR S.K",nname);
+printf("%c Relationship of Nominee : FATHER ",rname);
+printf("%d Nominee Age : 50 ",nage);
+printf("%d Nominee Mobile Number : 9372977301",n_n0);
+printf("%c Nominee Occupation : FARMER ",nom_oc);
+printf("%c Nominee Email : mohan124@gmail.com",nemail);
+printf("%d Nominee Aadhar Number : 7878983297379",aa);
+}
