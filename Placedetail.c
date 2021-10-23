d77f2bb99639b2a1e5d0b03ae5e5c9bb177c0819
sibiraj -20 <sibirajm.20cse@kongu.edu>
2021-10-23 12:05:32 +0530

    Placement Commit

---
+++ Placedetail.c
@@ -0,0 +1,14 @@
+void main ()
+{
+int mno,gpa1,gpa2,gpa3,cgpa;
+char sname,pmail,edmail,ds,sr;
+printf("%c Student : SIBIRAJ M",sname);
+printf("%d Mobile Number  : 7564889108",mno);
+printf("%c Personal Mailid : aab@gmail.com",pmail);
+printf("%d Number of arrears : NULL",sr);
+printf("%c GPA: 7.2",gpa1);
+printf("%c GPA: 7.2",gpa3);
+printf("%c GPA: 7.2",gpa3);
+printf("%c GPA: 7.2",gpa1);
+printf("%c CGPA: 7.2",cgpa);
+}
