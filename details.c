dd98ad680948c9d442c8aa5cec3d86f326c68b20
sibiraj -20 <sibirajm.20cse@kongu.edu>
2021-10-23 11:23:13 +0530

    First commit

---
+++ details.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main
+{
+int age,mob,a,blood;
+char name,rno,f_name,m_name,email,address;
+printf("%c Name : Sibiraj",name);
+printf ("%c Rollno :20CSR197",rno);
+printf("%cFather Name:Mohankumar.Sk",f_name);
+printf ("%cMother Name :Visalatchi.M",m_name);
+printf("%dAge : 18",age);
+printf("%dMobile Number: 7676728901",mob);
+printf("%cEmail : sibirajm.20cse@kongu.edu",email);
+printf ("%dBlood Group : AB+ve",blood);
+printf ("%cAddress : AAA",address);
+printf("%dAadhar Number :46377323687378",a);
+}
+
+
