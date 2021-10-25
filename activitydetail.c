4bc901da34cc02645c739c3d014bc7db9d3af7d9
sibiraj -20 <sibirajm.20cse@kongu.edu>
2021-10-25 19:16:31 +0530

    activity commit

---
+++ activitydetail.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main
+{
+int paper,project,out,p_out,pwon,hack; 
+string name,rno,vac,course ;
+printf ("%c  Rollno :20CSR197",rno);
+printf("%c Semester : 3",sem);
+printf("%c Name of paper presented inside KEC : ",paper );
+printf("%c Name of project  presented inside KEC : ",project );
+printf("%c Name of paper presented outside  KEC : ",out );
+printf("%c Name of project  presented outside KEC : ",p_out );
+printf("%c Name of paper presented inside KEC : ",paper );
+printf("%c No of prize won in Coding Carnival  : ",pwon );
+printf("%c No of prize won in Hackathon   : ",hack );
+}
