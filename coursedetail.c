e5f2c265959a69bcb755e1c94361dd6f480677ca
sibiraj -20 <sibirajm.20cse@kongu.edu>
2021-10-25 19:24:57 +0530

    Course complete

---
+++ coursedetail.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main
+{
+int one,vad,sem,no,np;
+char name,rno,vac,course ;
+printf ("%c  Rollno :20CSR197",rno);
+printf("%c Semester : 3",sem);
+printf ("%d No of one credit completed ",no);
+printf("%c Name one credit course : ",course );
+printf("%d No of value added course completed ",vad);
+printf("%c Name of one credit course completed ",vac);
+printf ("%d NPTEL result :",np);
+}
+
