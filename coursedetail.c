72386c46e50e5019d9fd53e7ebe442021ed6fff6
sibiraj-20 <91017207+sibiraj-20@users.noreply.github.com>
2021-10-26 19:35:13 +0530

    adding commit

--- coursedetail.c
+++ coursedetail.c
@@ -2,7 +2,7 @@
 void main
 {
 int one,vad,sem,no,np;
-char name,rno,vac,course ;
+char name,rno,vac,course,f ;
 printf ("%c  Rollno :20CSR197",rno);
 printf("%c Semester : 3",sem);
 printf ("%d No of one credit completed ",no);
@@ -10,5 +10,6 @@ printf("%c Name one credit course : ",course );
 printf("%d No of value added course completed ",vad);
 printf("%c Name of one credit course completed ",vac);
 printf ("%d NPTEL result :",np);
+printf("%c Adding new file :",f);
 }
 
